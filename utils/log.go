26733be0de7cb3e6142f8f68a63a878e2e3a8267
Reim-developer <contact.kaxtr@gmail.com>
2025-03-09 14:41:34 +0700

    Optimize code and remove interface in function parameters

--- utils/log.go
+++ utils/log.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-// Log, useful for display info
-func Log(format string, args ...interface{}) {
+// Log, useful for display info.
+func Log(format string, args ...any) {
 	fmt.Println(fmt.Sprintf(format, args...))
 }
 
-// Fatal, useful for display error
-func Fatal(format string, args ...interface{}) {
+// Fatal, useful for display error.
+func Fatal(format string, args ...any) {
 	fmt.Println(fmt.Sprintf(format, args...))
 }
