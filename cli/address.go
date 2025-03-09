cd0a3e68fa9c0fe01fbe4aa0e6fad3df2e9f5282
Reim-developer <contact.kaxtr@gmail.com>
2025-03-09 14:40:20 +0700

    fixed variables name, and bug logic

--- cli/address.go
+++ cli/address.go
@@ -8,25 +8,28 @@ import (
 )
 
 func AddressCommand(cli *cobra.Command) {
-	var website_url string
-	address_cmd := &cobra.Command{
+	var websiteURL string
+
+	addressCmd := &cobra.Command{
 		Use: "addr",
-		Run: func(cmd *cobra.Command, args []string) {
-			if website_url == "" {
+		Run: func(cmd *cobra.Command, _ []string) {
+			if websiteURL == "" {
 				utils.Log("Usage: mtrang addr --url <WEBSITE_URL>")
 				return
 			}
 
 			debug, _ := cmd.Flags().GetBool("debug")
 			if debug {
-				core.AddrLookup(website_url, false)
-			} else {
-				core.AddrLookup(website_url, true)
+				core.AddrLookup(websiteURL, false)
+				return
 			}
+
+			core.AddrLookup(websiteURL, true)
+
 		},
 	}
 
-	address_cmd.Flags().StringVarP(&website_url, "url", "u", "", "Website you want get lookup address")
-	address_cmd.Flags().BoolP("debug", "", false, "Enable debug mode")
-	cli.AddCommand(address_cmd)
+	addressCmd.Flags().StringVarP(&websiteURL, "url", "u", "", "Website you want get lookup address")
+	addressCmd.Flags().BoolP("debug", "", false, "Enable debug mode")
+	cli.AddCommand(addressCmd)
 }
