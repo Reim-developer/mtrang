e06e41af68583d71bdc33d74c55fb33481abd486
Reim-developer <contact.kaxtr@gmail.com>
2025-03-10 20:29:11 +0700

    Fixed main.go

--- main.go
+++ main.go
@@ -11,15 +11,16 @@ func main() {
 	cmd := &cobra.Command{
 		Use:   "mtrang",
 		Short: "Pentest Command Line Interface",
-
 		Run: func(_ *cobra.Command, _ []string) {
 			utils.Log("Try: mtrang help for more information")
 		},
 	}
 
+	cmd.DisableFlagParsing = true
 	cli.VersionCommand(cmd)
 	cli.AddressCommand(cmd)
 	cli.ScanPortCommand(cmd)
+	cli.HelpCommand(cmd)
 
 	if err := cmd.Execute(); err != nil {
 		utils.Fatal("Fatal: %s", err)
